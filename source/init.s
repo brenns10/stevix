6014dd7a2c013cb9520bcf91f18bdf31b2583140
Stephen Brennan <smb196@case.edu>
2017-04-01 17:11:08 -0400

    Annoyingly happy comment on _start

--- source/init.s
+++ source/init.s
@@ -1,3 +1,9 @@
+/*
+  Welcome! This is where it all starts. After initialization the GPU (ugh) loads
+  kernel.img and sets pc to 0x8000. Here we set up the stack pointer and invoke
+  our main() function. If the main function ever returns, we enter an infinite
+  loop.
+*/
   .section .init
   .globl _start
 _start:
