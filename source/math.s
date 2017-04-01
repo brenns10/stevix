05a78e107b342f679963e3af818fabce13366550
Stephen Brennan <smb196@case.edu>
2017-04-01 19:02:00 -0400

    Fix division operator

--- source/math.s
+++ source/math.s
@@ -6,7 +6,11 @@ r1: divisor
 r0: result
 */
   .globl divide_u32
+  .globl __aeabi_uidiv
+  .globl __aeabi_uidivmod
 divide_u32:
+__aeabi_uidiv:
+__aeabi_uidivmod:
   result .req r0
   remainder .req r1
   shift .req r2
@@ -14,21 +18,26 @@ divide_u32:
 
   clz shift, r1
   clz r3, r0
-  sub shift, shift, r3
+  subs shift, r3
   lsl current, r1, shift
   mov remainder, r0
   mov result, #0
+  blt $divide_u32_return
 
 $divide_u32_loop:
-  cmp shift, #0
-  blt $divide_u32_return
   cmp remainder, current
-  addge result, result, #1
-  subge remainder, current
-  sub shift, #1
-  lsr current, #1
-  lsl result, #1
-  b $divide_u32_loop
+  blt $divide_u32_loop_continue
+
+  add result, result, #1
+  subs remainder, current // short circuit if we evenly divided
+  lsleq result, shift
+  beq $divide_u32_return
+
+$divide_u32_loop_continue:
+  subs shift, #1
+  lsrge current, #1
+  lslge result, #1
+  bge $divide_u32_loop
 
 $divide_u32_return:
   .unreq current
@@ -36,19 +45,3 @@ $divide_u32_return:
   .unreq result
   .unreq remainder
   .unreq shift
-
-/*
-  Calls to these functions are emitted by the C compiler for the / operator and
-  the % operator. I have implemented them (they would normally be included in
-  a standard library of sorts) for my own convenience.
-*/
-  .globl __aeabi_uidiv
-__aeabi_uidiv:
-  b divide_u32
-
-  .globl __aeabi_uimod
-__aeabi_uimod:
-  push {pc}
-  bl divide_u32
-  mov r0, r1
-  pop {lr}
